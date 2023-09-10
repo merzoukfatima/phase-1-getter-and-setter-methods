f3381592448e3342d9f3777fbbe19f081486928a
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-09-10 02:03:14 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your Circle class here
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  set diameter(diameter) {
+    this.radius = diameter / 2;
+  }
+  get diameter() {
+    return this.radius * 2;
+  }
+  set circumference(circumference) {
+    this.radius = circumference / Math.PI / 2;
+  }
+  get circumference() {
+    return this.diameter * Math.PI;
+  }
+  set area(area) {
+    this.radius = Math.sqrt(area / Math.PI);
+  }
+  get area() {
+    return Math.pow(this.radius, 2) * Math.PI;
+  }
+}
